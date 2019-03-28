7043ce1c202774b37fb9f9ab757f470ff85c8d6f
Amie DeFrain <47186450+moomoocat@users.noreply.github.com>
2019-03-28 13:49:58 -0700

    Add files via upload

---
+++ assignment03 - JavaScript/scripts/script.js
@@ -0,0 +1,188 @@
+	
+//1) Convert to camel case:
+
+//var someMonth;
+
+//var someMonth = prompt('What month is it?');
+//		document.getElementById('p1').innerHTML = 'The month is ' + someMonth + '.';
+		
+//const CURRENTMONTH = currentMonth;
+	
+//var summerMonth = ['June', 'July', 'August'];
+
+//function myLibraryAFunction () {
+//	
+//}
+
+//2) Literal Expressions: 		 
+//var num = 1; 	   //numeric
+//var alone = 'one'; //string	  
+//var winter = true; //boolean
+
+			   
+//3) Two complex/variable Expressions: 
+//var a = 5;
+//var b = 6;
+//var c = 7;
+//var sum = (a+b)*c;
+//console.log(sum); //77		   
+
+//var a = 5;
+//var b = 6;
+//var c = 7;
+//var sum = (a+b)/c;
+//console.log(sum); //1.5714285714285714
+
+	
+//4) 9 variables:
+//var strFirstName;
+//var strLastName;
+//var strAddress;
+//var strCity;
+//var strState;
+//var numZipCode;
+//var numYourAge;
+//var strReferralSource;
+//var boolMayWeContactYou;			   
+
+						   
+//5) Three methods for declaring and assigning variables:
+//strFirstName = 'Amie';
+//strLastName = 'DeFrain';
+//strAddress = '825 S. Kalmia St.';
+//strCity = 'Escondido';
+//strState = 'California';
+//numZipCode = 92025;
+//numYourAge = 49;
+//strReferralSource = 'SDCE';
+//boolMayWeContactYou = true; //yes
+
+	
+	
+//6) Create three variables:
+//var red = 1+'string';
+//console.log(red);       //1string
+//
+//var blue = true+'string';
+//console.log(blue);      //truestring
+//
+//var number = 1*2;
+//console.log(number);    //2
+
+	
+	
+//7) Create two variables:
+//var cat1 = 'Bumble';
+//var cat2 = 'Moo';
+//document.body.innerHTML = 'My cats are ' + cat1 + ' and ' + cat2 + '.';
+
+	
+
+//8) Fix string:
+//The apostrophe is the problem. You would need to add a "\" in "I\'m"
+
+	
+
+//9) Minimum amount of code you need to store:
+//Using prompt to install text/value to variable:
+//var strName = prompt('Please enter your name ');
+//document.body.innerHTML = 'Your name is ' + strName;
+
+	
+	
+//10) typeof operator on four literals:
+//let whatAmI = 'Playing around with typeof.';
+//console.log(typeof whatAmI);  //string
+//
+//let whatAmI2 = 123456;
+//console.log(typeof whatAmI2);  //number
+//
+//let whatAmI3 = true;
+//console.log(typeof whatAmI3);  //boolean
+
+	
+	
+//11) Alert box:
+//alert('Hello Amie DeFrain, welcome to the 644 class!');
+
+
+	
+//12) 
+//	var name = 'Amie DeFrain';
+//	alert('Hello ' + strName + ', welcome to the 644 class!');
+
+	
+	
+//13)
+//  var course = 'JavaScript';
+//	alert(course);
+
+	
+	
+//14) 
+//  alert('Hello Emily Kay.\nWelcome to the 644 class!');
+
+
+//15)
+//var name = prompt('Please enter your name ');
+//alert('Hello ' + name + '.\nWelcome to the 644 class!');
+
+	
+//16)
+//var course = prompt('Please enter your course name ');
+//alert('Hello Amie DeFrain.\nWelcome to the ' + course + ' class!');
+	
+	
+//17)
+//	var x = 10;
+//	var y = 20;
+//	console.log(x+y);
+	
+	
+//18)
+//var x = 20;
+//x += 20; 		//x = x+20
+//console.log(x);
+
+
+	
+//19)
+//	var x = 20;
+//	x *= 5      //x = x*5
+//	console.log(x);
+	
+
+//20)
+//var x = 20 % 3;
+//console.log(x);
+	
+
+	
+//21)
+//	var x = 8;
+//	x += 1;
+//	console.log(x);
+	
+	
+//22)
+//	var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
+	
+//23)
+//var a=2, b=2, c=3, d=5;
+//if(a==b || c==d) {
+//	console.log('yes, at least one of them is a match!');
+//}
+	
+	
+//24)
+//var a=2, b=2, c=3, d=5;
+//if(a==b && c==d) {
+//	console.log('yes, both match!');
+//} else {
+//    console.log('no, both do not match!');
+//}
+
+	
+//25)
+//var widget = new Object();
+//console.log(typeof widget);
